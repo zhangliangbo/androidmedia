1b1e944523c25eede57b02589a3efef4f5462e31
zhangliangbo <2472110501@qq.com>
2018-04-28 14:56:10 +0800

    简化CameraOne

--- library/src/main/java/com/mcivicm/media/helper/CameraOneHelper.java
+++ library/src/main/java/com/mcivicm/media/helper/CameraOneHelper.java
@@ -258,56 +258,6 @@ public class CameraOneHelper {
     }
 
     /**
-     * 将图片按照某个角度进行旋转
-     *
-     * @param bm     需要旋转的图片
-     * @param degree 旋转角度
-     * @return 旋转后的图片
-     */
-    public static Bitmap rotateBitmapByDegree(Bitmap bm, int degree) {
-        Bitmap returnBm = null;
-        // 根据旋转角度，生成旋转矩阵
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degree);
-        try {
-            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
-            returnBm = Bitmap.createBitmap(
-                    bm,
-                    0,
-                    0,
-                    bm.getWidth(),
-                    bm.getHeight(),
-                    matrix,
-                    true
-            );
-        } catch (OutOfMemoryError e) {
-
-        }
-        if (returnBm == null) {
-            returnBm = bm;
-        }
-        if (bm != returnBm) {
-            bm.recycle();
-        }
-        return returnBm;
-    }
-
-    public static void save(Bitmap bitmap, String filePath) {
-        File file = new File(filePath);
-        if (!file.getParentFile().exists()) {
-            file.getParentFile().mkdirs(); // 创建文件夹
-        }
-        try {
-            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos); // 向缓冲区之中压缩图片
-            bos.flush();
-            bos.close();
-        } catch (Exception e) {
-            //ignore
-        }
-    }
-
-    /**
      * 分辨率最高
      *
      * @param list
